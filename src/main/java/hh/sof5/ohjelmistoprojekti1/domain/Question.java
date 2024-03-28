3e3e2e98e0b93a443b5955387e4bb154c7b93d81
TumSim <tuomas.simoinen@gmail.com>
2024-03-28 13:58:18 +0200

    test

--- src/main/java/hh/sof5/ohjelmistoprojekti1/domain/Question.java
+++ src/main/java/hh/sof5/ohjelmistoprojekti1/domain/Question.java
@@ -1,8 +1,67 @@
 package hh.sof5.ohjelmistoprojekti1.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "Question")
+
 public class Question {
+
+    // Attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long questionid;
     private String question;
+
+    @ManyToMany
+    @JsonIgnoreProperties("questions")
+    @JoinColumn(name = "queryid") //FK
+    private Query query;
+
+    // Constructors
+    public Question() {
+        this.questionid = null;
+        this.question = null;
+    }
+    
+    public Question(String question) {
+        this.question = question;
+    }
+
+    // Getters
+    public Long getQuestionid() {
+        return questionid;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    // Setters
+    public void setQuestionid(Long questionid) {
+        this.questionid = questionid;
+    }
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+    // toString
+    
+    @Override
+    public String toString() {
+        return "Question [questionid=" + questionid + ", question=" + question + "]";
+    }
+
+    
+    
 
     
 }
---
+++ src/main/java/hh/sof5/ohjelmistoprojekti1/domain/QuestionRepository.java
@@ -0,0 +1,7 @@
+package hh.sof5.ohjelmistoprojekti1.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface QuestionRepository extends CrudRepository<Question, Long> {
+
+}
