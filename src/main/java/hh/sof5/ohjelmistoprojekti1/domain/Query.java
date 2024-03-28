97dfdaea3f3dd628ffea1118d7c7919e85f3235d
Mikkosar <mikkosar12@gmail.com>
2024-03-28 13:43:33 +0200

    Queqy entity addedd

--- src/main/java/hh/sof5/ohjelmistoprojekti1/domain/Query.java
+++ src/main/java/hh/sof5/ohjelmistoprojekti1/domain/Query.java
@@ -2,10 +2,43 @@ package hh.sof5.ohjelmistoprojekti1.domain;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Query {
+
+    //Attributes
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long queryid;
     private String name;
     private String desc;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "Question")
+    @JsonIgnoreProperties ("query")
     private List <Question> questions;
+
+    //Constructor
+
+    public Query(String name, String desc, List<Question> questions) {
+        this.name = name;
+        this.desc = desc;
+        this.questions = questions;
+    }
+
+    //Null Constructor
+
+    public Query(){
+
+    }
+    
 
 }
--- src/main/java/hh/sof5/ohjelmistoprojekti1/domain/Question.java
+++ src/main/java/hh/sof5/ohjelmistoprojekti1/domain/Question.java
@@ -3,4 +3,6 @@ package hh.sof5.ohjelmistoprojekti1.domain;
 public class Question {
     private Long questionid;
     private String question;
+
+    
 }
