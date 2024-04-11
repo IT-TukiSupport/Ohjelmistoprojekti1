76825cedb0ae617ac2d9c33289ba62e3e3154c5a
pinzati <pinskutirkkonen@hotmail.com>
2024-04-11 12:41:38 +0300

    AnswerRepository lisätty

---
+++ src/main/java/hh/sof5/ohjelmistoprojekti1/domain/AnswerRepository.java
@@ -0,0 +1,7 @@
+package hh.sof5.ohjelmistoprojekti1.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AnswerRepository extends CrudRepository<Answer, Long> {
+
+}
